ca8fb664300662b1cbf83556c150fc292406a91e
Administrator <864508339@qq.com>
2016-05-27 00:56:36 +0800

    初步圆角圆形ImageView

--- app/src/main/java/com/horizon/android/widget/XCRoundImageView.java
+++ app/src/main/java/com/horizon/android/widget/XCRoundImageView.java
@@ -8,32 +8,28 @@ import android.graphics.Paint;
 import android.graphics.PorterDuff;
 import android.graphics.PorterDuffXfermode;
 import android.graphics.RectF;
+import android.graphics.Xfermode;
 import android.graphics.drawable.Drawable;
 import android.util.AttributeSet;
 import android.widget.ImageView;
+
+import com.horizon.android.util.log.LogUtils;
 
 import java.lang.ref.WeakReference;
 
 public class XCRoundImageView extends ImageView {
 
-    private TYPE mType = TYPE.CIRCLE;
+    private static final int CIRCLE = 0;
+    private static final int ROUND = 1;
+    private static final int OVAL = 2;
+
+    private int mType;
     private Paint mPaint;
+    private Xfermode mXfermode = new PorterDuffXfermode(PorterDuff.Mode.DST_IN);
 
     private Bitmap mBmp;
     private WeakReference<Bitmap> mBuffer;
     private int mRoundRadius;
-
-    public static enum TYPE {
-        CIRCLE("circle"),
-        ROUND("round"),
-        OVAL("oval");
-
-        private String value;
-
-        private TYPE(String value){
-            this.value = value;
-        }
-    }
 
     public XCRoundImageView(Context context) {
         this(context, null);
@@ -42,6 +38,7 @@ public class XCRoundImageView extends ImageView {
     public XCRoundImageView(Context context, AttributeSet attrs) {
         super(context, attrs);
 
+        mType = CIRCLE;
         mPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
     }
 
@@ -49,7 +46,7 @@ public class XCRoundImageView extends ImageView {
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
 
-        if(mType == TYPE.CIRCLE){
+        if(mType == CIRCLE){
             int width = Math.min(getMeasuredHeight(), getMeasuredWidth());
             setMeasuredDimension(width, width);
         }
@@ -59,33 +56,39 @@ public class XCRoundImageView extends ImageView {
     protected void onDraw(Canvas canvas) {
         Bitmap bmp = mBuffer == null ? null : mBuffer.get();
         if(bmp == null || bmp.isRecycled()){
-            bmp = Bitmap.createBitmap(getWidth(), getHeight(), Bitmap.Config.ARGB_8888);
-            Canvas drawCanvas = new Canvas(bmp);
-
             Drawable drawable = getDrawable();
-            int dWidth = drawable.getIntrinsicWidth();
-            int dHeight = drawable.getIntrinsicHeight();
-            float scale = 1f;
-            if(mType == TYPE.CIRCLE){
-                scale = getWidth() * 1f / Math.min(dWidth, dHeight);
-                drawable.setBounds((dWidth - (int)(scale*dWidth))/2, (dHeight - (int)(scale*dHeight))/2, (int)(scale*dWidth), (int)(scale*dHeight));
-            } else if(mType == TYPE.ROUND || mType == TYPE.OVAL){
-                scale = Math.max(getWidth() * 1f / dWidth, getHeight() * 1f / dHeight);
-                drawable.setBounds(0, 0, (int)(scale*dWidth), (int)(scale*dHeight));
-            }
-            drawable.draw(drawCanvas);
+            if(drawable != null) {
+                int dWidth = drawable.getIntrinsicWidth();
+                int dHeight = drawable.getIntrinsicHeight();
 
-            if(mBmp == null || mBmp.isRecycled()){
-                mBmp = getDrawBitmap();
-            }
-            mPaint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.DST_IN));
-            canvas.drawBitmap(mBmp, 0, 0, mPaint);
+                bmp = Bitmap.createBitmap(getWidth(), getHeight(), Bitmap.Config.ARGB_8888);
+                Canvas drawCanvas = new Canvas(bmp);
 
-            mBuffer = new WeakReference<Bitmap>(bmp);
+                float scale = 1.0f;
+                if (mType == CIRCLE) {
+                    scale = getWidth() * 1.0f / Math.min(dWidth, dHeight);
+                } else if (mType == ROUND || mType == OVAL) {
+                    scale = Math.max(getWidth() * 1.0f / dWidth, getHeight() * 1.0f / dHeight);
+                }
+                drawable.setBounds(0, 0, (int) (scale * dWidth), (int) (scale * dHeight));
+                drawable.draw(drawCanvas);
+
+                if (mBmp == null || mBmp.isRecycled()) {
+                    mBmp = getDrawBitmap();
+                }
+                mPaint.reset();
+                mPaint.setFilterBitmap(false);
+                mPaint.setXfermode(mXfermode);
+                drawCanvas.drawBitmap(mBmp, 0, 0, mPaint);
+                mPaint.setXfermode(null);
+                canvas.drawBitmap(bmp, 0, 0, null);
+
+                mBuffer = new WeakReference<Bitmap>(bmp);
+            }
+        } else {
+            mPaint.setXfermode(null);
+            canvas.drawBitmap(bmp, 0, 0, mPaint);
         }
-
-        mPaint.setXfermode(null);
-        canvas.drawBitmap(bmp, 0, 0, mPaint);
 
     }
 
@@ -96,12 +99,12 @@ public class XCRoundImageView extends ImageView {
         Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
         paint.setColor(Color.BLACK);
 
-        if(mType == TYPE.CIRCLE) {
+        if(mType == CIRCLE) {
             int radius = getWidth() / 2;
             canvas.drawCircle(radius, radius, radius, paint);
-        } else if(mType == TYPE.ROUND) {
+        } else if(mType == ROUND) {
             canvas.drawRoundRect(new RectF(0, 0, getWidth(), getHeight()), mRoundRadius, mRoundRadius, paint);
-        } else if(mType == TYPE.OVAL) {
+        } else if(mType == OVAL) {
             canvas.drawOval(new RectF(0, 0, getWidth(), getHeight()), paint);
         }
 
