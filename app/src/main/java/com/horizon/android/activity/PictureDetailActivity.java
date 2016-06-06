d9ff2fc751bb65ea96d99603b32e506d9678789e
Administrator <864508339@qq.com>
2016-06-06 23:14:18 +0800

    大图查看优化

--- app/src/main/java/com/horizon/android/activity/PictureDetailActivity.java
+++ app/src/main/java/com/horizon/android/activity/PictureDetailActivity.java
@@ -40,6 +40,7 @@ public class PictureDetailActivity extends Activity {
     ProgressBar progressLoad;
 
     SmallPicInfo smallPicInfo;
+    PhotoViewAttacher attacher;
 
     private static final int DURATION = 250;
     private int screenWidth, screenHeight;
@@ -61,119 +62,124 @@ public class PictureDetailActivity extends Activity {
 
         smallPicInfo = (SmallPicInfo) getIntent().getSerializableExtra(Constants.BUNDLE_PIC_INFOS);
 
-        final PhotoViewAttacher attacher = new PhotoViewAttacher(ivDetail);
+        attacher = new PhotoViewAttacher(ivDetail);
 
         DiskCache diskCache = Application.getInstance().getImageLoader().getDiskCache();
         File file = diskCache.get(smallPicInfo.url);
         if (userCache && file != null && file.exists()) {
             userCache = false;
-            LogUtils.e("有缓存");
             Bitmap bitmap = BitmapFactory.decodeFile(file.getAbsolutePath());
-            LogUtils.e("bitmap is null? " + (bitmap == null));
-
-            int w = bitmap.getWidth();
-            int h = bitmap.getHeight();
-            float scale = screenWidth * 1.0f / w;
-
-            RelativeLayout.LayoutParams lp = (RelativeLayout.LayoutParams) ivDetail.getLayoutParams();
-            lp.height = (int) (scale * h);
-            lp.width = screenWidth;
-            ivDetail.setLayoutParams(lp);
-            ivDetail.setImageBitmap(bitmap);
-            attacher.update();
-
-            float scaleX = smallPicInfo.width * 1.0f / lp.width;
-            float scaleY = smallPicInfo.height * 1.0f / lp.height;
-
-            int deltaX = smallPicInfo.left - (screenWidth - lp.width) / 2;
-            int deltaY = smallPicInfo.top - (screenHeight - lp.height) / 2;
-
-            ivDetail.setPivotX(0);
-            ivDetail.setPivotY(0);
-            ivDetail.setScaleX(scaleX);
-            ivDetail.setScaleY(scaleY);
-
-            ivDetail.setTranslationX(deltaX);
-            ivDetail.setTranslationY(deltaY);
-
-            ivDetail.animate()
-                    .scaleX(1f).scaleY(1f)
-                    .translationX(0).translationY(0)
-                    .setDuration(DURATION).setListener(new SimpleAnimatorListener() {
-                @Override
-                public void onAnimationEnd(Animator animation) {
-                    RelativeLayout.LayoutParams lp = (RelativeLayout.LayoutParams) ivDetail.getLayoutParams();
-                    lp.height = Application.getInstance().SCREENHEIGHT;
-                    lp.width = Application.getInstance().SCREENWIDTH;
-                    ivDetail.setLayoutParams(lp);
-                }
-            });
-
+            loadOnCache(bitmap);
         } else {
             userCache = true;
-            LogUtils.e("无缓存");
-            RelativeLayout.LayoutParams lp = (RelativeLayout.LayoutParams) ivDetail.getLayoutParams();
-            lp.width = smallPicInfo.width;
-            lp.height = smallPicInfo.height;
-            ivDetail.setLayoutParams(lp);
-
-            Bitmap small = BitmapFactory.decodeByteArray(smallPicInfo.bmp, 0, smallPicInfo.bmp.length);
-            ivDetail.setImageBitmap(small);
-
-            int smallDeltaX = smallPicInfo.left - (screenWidth - smallPicInfo.width) / 2;
-            int smallDeltaY = smallPicInfo.top - (screenHeight - smallPicInfo.height + DisplayUtils.getStatusBarHeight(PictureDetailActivity.this)) / 2;
-
-            ivDetail.setPivotX(0);
-            ivDetail.setPivotY(0);
-            ivDetail.setTranslationX(smallDeltaX);
-            ivDetail.setTranslationY(smallDeltaY);
-
-            ivDetail.animate().translationX(0).translationY(0).setDuration(DURATION).setListener(new SimpleAnimatorListener() {
-                @Override
-                public void onAnimationEnd(Animator animation) {
-                    progressLoad.setVisibility(View.VISIBLE);
-
-                    DisplayImageOptions options = new DisplayImageOptions.Builder()
-                            .cacheOnDisk(true)
-                            .bitmapConfig(Bitmap.Config.RGB_565)
-                            .build();
-
-                    Application.getInstance().getImageLoader().displayImage(smallPicInfo.url, ivDetail, options, new SimpleImageLoadingListener() {
-
-                        @Override
-                        public void onLoadingComplete(String imageUri, View view, Bitmap loadedImage) {
-                            progressLoad.setVisibility(View.GONE);
-                            attacher.update();
-
-                            float scaleX = screenWidth * 1f / smallPicInfo.width;
-                            float scaleY = screenHeight * 1f / smallPicInfo.height;
-                            float scale = Math.min(scaleX, scaleY);
-
-                            ivDetail.setPivotX(smallPicInfo.width / 2);
-                            ivDetail.setPivotY(smallPicInfo.height / 2);
-
-                            ivDetail.animate().scaleX(scale).scaleY(scale).setDuration(DURATION).setListener(new SimpleAnimatorListener() {
-                                @Override
-                                public void onAnimationEnd(Animator animation) {
-                                    ivDetail.setScaleX(1f);
-                                    ivDetail.setScaleY(1f);
-                                    RelativeLayout.LayoutParams lp = (RelativeLayout.LayoutParams) ivDetail.getLayoutParams();
-                                    lp.height = screenHeight;
-                                    lp.width = screenWidth;
-                                    ivDetail.setLayoutParams(lp);
-                                }
-                            });
-                        }
-
-                        @Override
-                        public void onLoadingFailed(String imageUri, View view, FailReason failReason) {
-                            progressLoad.setVisibility(View.GONE);
-                        }
-                    });
-                }
-            });
+            loadOnNetwork();
         }
     }
+
+    private void loadOnNetwork(){
+        RelativeLayout.LayoutParams lp = (RelativeLayout.LayoutParams) ivDetail.getLayoutParams();
+        lp.width = smallPicInfo.width;
+        lp.height = smallPicInfo.height;
+        ivDetail.setLayoutParams(lp);
+
+        Bitmap small = BitmapFactory.decodeByteArray(smallPicInfo.bmp, 0, smallPicInfo.bmp.length);
+        ivDetail.setImageBitmap(small);
+
+        int smallDeltaX = smallPicInfo.left - (screenWidth - smallPicInfo.width) / 2;
+        int smallDeltaY = smallPicInfo.top - (screenHeight - smallPicInfo.height + DisplayUtils.getStatusBarHeight(PictureDetailActivity.this)) / 2;
+
+        ivDetail.setPivotX(0);
+        ivDetail.setPivotY(0);
+        ivDetail.setTranslationX(smallDeltaX);
+        ivDetail.setTranslationY(smallDeltaY);
+
+        ivDetail.animate().translationX(0).translationY(0).setDuration(DURATION).setListener(new SimpleAnimatorListener() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                progressLoad.setVisibility(View.VISIBLE);
+
+                DisplayImageOptions options = new DisplayImageOptions.Builder()
+                        .cacheOnDisk(true)
+                        .bitmapConfig(Bitmap.Config.RGB_565)
+                        .build();
+
+                Application.getInstance().getImageLoader().displayImage(smallPicInfo.url, ivDetail, options, new SimpleImageLoadingListener() {
+
+                    @Override
+                    public void onLoadingComplete(String imageUri, View view, Bitmap loadedImage) {
+                        progressLoad.setVisibility(View.GONE);
+                        attacher.update();
+
+                        float scaleX = screenWidth * 1f / smallPicInfo.width;
+                        float scaleY = screenHeight * 1f / smallPicInfo.height;
+                        float scale = Math.min(scaleX, scaleY);
+
+                        ivDetail.setPivotX(smallPicInfo.width / 2);
+                        ivDetail.setPivotY(smallPicInfo.height / 2);
+
+                        ivDetail.animate().scaleX(scale).scaleY(scale).setDuration(DURATION).setListener(new SimpleAnimatorListener() {
+                            @Override
+                            public void onAnimationEnd(Animator animation) {
+                                ivDetail.setScaleX(1f);
+                                ivDetail.setScaleY(1f);
+                                setImageViewMatch();
+                            }
+                        });
+                    }
+
+                    @Override
+                    public void onLoadingFailed(String imageUri, View view, FailReason failReason) {
+                        progressLoad.setVisibility(View.GONE);
+                    }
+                });
+            }
+        });
+    }
+
+    private void loadOnCache(Bitmap bitmap){
+        int w = bitmap.getWidth();
+        int h = bitmap.getHeight();
+        float scale = screenWidth * 1.0f / w;
+
+        RelativeLayout.LayoutParams lp = (RelativeLayout.LayoutParams) ivDetail.getLayoutParams();
+        lp.height = (int) (scale * h);
+        lp.width = screenWidth;
+        ivDetail.setLayoutParams(lp);
+        ivDetail.setImageBitmap(bitmap);
+        attacher.update();
+
+        float scaleX = smallPicInfo.width * 1.0f / lp.width;
+        float scaleY = smallPicInfo.height * 1.0f / lp.height;
+
+        int deltaX = smallPicInfo.left - (screenWidth - lp.width) / 2;
+        int deltaY = smallPicInfo.top - (screenHeight - lp.height) / 2;
+
+        ivDetail.setPivotX(0);
+        ivDetail.setPivotY(0);
+        ivDetail.setScaleX(scaleX);
+        ivDetail.setScaleY(scaleY);
+
+        ivDetail.setTranslationX(deltaX);
+        ivDetail.setTranslationY(deltaY);
+
+        ivDetail.animate()
+                .scaleX(1f).scaleY(1f)
+                .translationX(0).translationY(0)
+                .setDuration(DURATION).setListener(new SimpleAnimatorListener() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                setImageViewMatch();
+            }
+        });
+    }
+
+    private void setImageViewMatch(){
+        RelativeLayout.LayoutParams lp = (RelativeLayout.LayoutParams) ivDetail.getLayoutParams();
+        lp.height = Application.getInstance().SCREENHEIGHT;
+        lp.width = Application.getInstance().SCREENWIDTH;
+        ivDetail.setLayoutParams(lp);
+    }
+
 
     @Override
     public void onBackPressed() {
